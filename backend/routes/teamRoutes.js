90d09489a3045f75ec623ba930aba4fbdaa2a414
Luana Morais <104162761+LuanaMorais21@users.noreply.github.com>
2024-08-21 20:41:58 -0300

    Update teamRoutes.js

--- backend/routes/teamRoutes.js
+++ backend/routes/teamRoutes.js
@@ -1,5 +1,4 @@
 const express = require('express')
-<<<<<<< HEAD
 const router = express.Router()
 const teamController = require('../controllers/teamController')
 
@@ -14,14 +13,6 @@ router.put('/team/:id', teamController.updateTeam)
 
 // Rota para deletar um team
 router.delete('/team/:id', teamController.deleteTeam)
-=======
-const teamController = require('../controllers/teamController')
-const router = express.Router()
 
-router.post('/members', teamController.createTeamMember)
-router.get('/members', teamController.getTeamMembers)
-router.put('/members/:id', teamController.updateTeamMember)
-router.delete('/members/:id', teamController.deleteTeamMember)
->>>>>>> develop
 
 module.exports = router
