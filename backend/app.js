fb3b3a00f466c0be16bbb9e4b296b0c6a78f2472
Luana Morais <104162761+LuanaMorais21@users.noreply.github.com>
2024-08-21 20:40:16 -0300

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -2,15 +2,11 @@ const express = require('express')
 const cors = require('cors')
 const dotenv = require('dotenv')
 const bodyParser = require('body-parser')
-<<<<<<< HEAD
+
 const membersRoutes = require('./routes/membersRoutes')
 const taskRoutes = require('./routes/taskRoutes')
 const teamRoutes = require('./routes/teamRoutes')
-=======
-const teamRoutes = require('./routes/teamRoutes')
-const taskRoutes = require('./routes/taskRoutes')
-const roleRoutes = require('./routes/roleRoutes')
->>>>>>> develop
+
 
 const app = express()
 
@@ -18,15 +14,11 @@ app.use(cors())
 app.use(express.json())
 app.use(bodyParser.json())
 
-<<<<<<< HEAD
+
 app.use('/api', membersRoutes)
 app.use('/api', taskRoutes)
 app.use('/api', teamRoutes)
-=======
-app.use('/api', teamRoutes)
-app.use('/api', taskRoutes)
-app.use('/api', roleRoutes)
->>>>>>> develop
+
 
 const PORT = process.env.PORT || 5000
 
